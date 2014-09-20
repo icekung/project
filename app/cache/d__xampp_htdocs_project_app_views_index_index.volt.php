5f36c95f0f2d9c6fe1eb606dea37120314274ab5
icekung <icekung.1920@gmail.com>
2014-09-20 14:08:05 +0700

    start

---
+++ app/cache/d__xampp_htdocs_project_app_views_index_index.volt.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+
+ <!--bootstap-->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/bootstrap-theme.min.css" rel="stylesheet">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<div class="container">
+<img src="img/Head.jpg"class="img-responsive" alt="Responsive image">
+</div>
+<body>
+
+<div class="container">
+<div class="well">
+<br><br><br>
+<form class="form-horizontal" action='login/index'>
+  <div class="form-group">
+    <div class="col-sm-offset-4 col-sm-4">
+      Username:<input type="Username" class="form-control" id="inputUser" placeholder="Username">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-4 col-sm-4">
+      Password:<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-4 col-sm-10">
+      
+    </div>
+  </div>
+  <div class="form-group">
+
+	<center> 
+      <button type="submit" class="btn btn-default" >Login</button></center>
+    </div>
+  </div>
+</form>
+</br></br></br>
+</div>
+</body>
+</head></html>
---
+++ app/controllers/LoginController.php
@@ -0,0 +1,8 @@
+<?php
+
+class LoginController extends ControllerBase {
+  public function indexAction()
+    {
+      
+    }
+  }
